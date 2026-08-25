f1b847386ace8645af8c52632d8c8e886efcd4c2
Eerang2 <dlwlsdn8022@gmail.com>
2026-08-25 23:53:10 +0900

    프로젝트 생성

---
+++ src/main/java/techlog/TechlogApplication.java
@@ -0,0 +1,13 @@
+package techlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/techlog/TechlogApplicationTests.java
@@ -0,0 +1,13 @@
+package techlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
